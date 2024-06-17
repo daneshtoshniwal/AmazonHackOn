6231c6f35da836c70344b54ee87bcacd26529b77
daneshtoshniwal <daneshtoshniwal0@gmail.com>
2024-06-17 10:53:03 +0530

    initial commit

---
+++ project/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+
+      </div>
+      <div className="categoriesbar">
+
+      </div>
+      <div className="body">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
